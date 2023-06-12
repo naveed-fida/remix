4685a9269bd1fb101992722539387ba83ac3c910
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2023-06-13 09:17:49 +1000

    fix(dev): cache PostCSS for CSS Modules

--- packages/remix-dev/compiler/plugins/cssModuleImports.ts
+++ packages/remix-dev/compiler/plugins/cssModuleImports.ts
@@ -4,7 +4,10 @@ import fse from "fs-extra";
 import postcss from "postcss";
 import postcssModules from "postcss-modules";
 
-import { loadPostcssPlugins } from "../utils/postcss";
+import {
+  loadPostcssPlugins,
+  populateDependenciesFromMessages,
+} from "../utils/postcss";
 import type { Context } from "../context";
 
 const pluginName = "css-modules-plugin";
@@ -15,11 +18,11 @@ const compiledCssFilter = /\?css-modules-plugin-compiled-css$/;
 
 interface PluginData {
   resolveDir: string;
-  compiledCss: string;
+  css: string;
 }
 
 export const cssModulesPlugin = (
-  { config, options }: Context,
+  { config, options, fileWatchCache }: Context,
   { outputCss }: { outputCss: boolean }
 ): Plugin => {
   return {
@@ -47,53 +50,88 @@ export const cssModulesPlugin = (
         let { path: absolutePath } = args;
         let resolveDir = path.dirname(absolutePath);
 
-        let fileContents = await fse.readFile(absolutePath, "utf8");
-        let exports: Record<string, string> = {};
+        let cacheKey = `css-module:${absolutePath}`;
+        let { cacheValue } = await fileWatchCache.getOrSet(
+          cacheKey,
+          async () => {
+            let fileContents = await fse.readFile(absolutePath, "utf8");
+            let exports: Record<string, string> = {};
 
-        let { css: compiledCss } = await postcss([
-          ...postcssPlugins,
-          postcssModules({
-            generateScopedName:
-              options.mode === "production"
-                ? "[hash:base64:5]"
-                : "[name]__[local]__[hash:base64:5]",
-            getJSON: function (_, json) {
-              exports = json;
-            },
-            async resolve(id, importer) {
-              return (
-                await build.resolve(id, {
-                  resolveDir: path.dirname(importer),
-                  kind: "require-resolve",
-                })
-              ).path;
-            },
-          }),
-        ]).process(fileContents, {
-          from: absolutePath,
-          to: absolutePath,
-        });
+            let fileDependencies = new Set<string>([absolutePath]);
+            let globDependencies = new Set<string>();
 
-        // Each .module.css file ultimately resolves as a JS file that imports
-        // a virtual CSS file containing the compiled CSS, and exports the
-        // object that maps local names to generated class names. The compiled
-        // CSS file contents are passed to the virtual CSS file via pluginData.
-        let contents = [
-          outputCss
-            ? `import "./${path.basename(absolutePath)}${compiledCssQuery}";`
-            : null,
-          `export default ${JSON.stringify(exports)};`,
-        ]
-          .filter(Boolean)
-          .join("\n");
+            let { css, messages } = await postcss([
+              ...postcssPlugins,
+              postcssModules({
+                generateScopedName:
+                  options.mode === "production"
+                    ? "[hash:base64:5]"
+                    : "[name]__[local]__[hash:base64:5]",
+                getJSON: function (_, json) {
+                  exports = json;
+                },
+                async resolve(id, importer) {
+                  let resolvedPath = (
+                    await build.resolve(id, {
+                      resolveDir: path.dirname(importer),
+                      kind: "require-resolve",
+                    })
+                  ).path;
+
+                  // Since postcss-modules doesn't add `dependency` messages the
+                  // way other plugins do, we mark any files that are passed to
+                  // the `resolve` callback as dependencies of this CSS Module
+                  fileDependencies.add(resolvedPath);
+
+                  return resolvedPath;
+                },
+              }),
+            ]).process(fileContents, {
+              from: absolutePath,
+              to: absolutePath,
+            });
+
+            // Since we're using arbitrary PostCSS plugins, we need to manage
+            // dependencies other than those managed by postcss-modules
+            populateDependenciesFromMessages({
+              messages,
+              fileDependencies,
+              globDependencies,
+            });
+
+            let jsWithoutCssImport = `export default ${JSON.stringify(
+              exports
+            )};`;
+
+            // Each .module.css file ultimately resolves as a JS file that imports
+            // a virtual CSS file containing the compiled CSS, and exports the
+            // object that maps local names to generated class names. The compiled
+            // CSS file contents are passed to the virtual CSS file via pluginData.
+            let jsWithCssImport = [
+              `import "./${path.basename(absolutePath)}${compiledCssQuery}";`,
+              jsWithoutCssImport,
+            ].join("\n");
+
+            return {
+              cacheValue: {
+                jsWithCssImport,
+                jsWithoutCssImport,
+                css,
+              },
+              fileDependencies,
+            };
+          }
+        );
+
+        let { jsWithCssImport, jsWithoutCssImport, css } = cacheValue;
 
         let pluginData: PluginData = {
           resolveDir,
-          compiledCss,
+          css,
         };
 
         return {
-          contents,
+          contents: outputCss ? jsWithCssImport : jsWithoutCssImport,
           loader: "js" as const,
           pluginData,
         };
@@ -112,11 +150,11 @@ export const cssModulesPlugin = (
 
       build.onLoad({ filter: compiledCssFilter, namespace }, async (args) => {
         let pluginData: PluginData = args.pluginData;
-        let { resolveDir, compiledCss } = pluginData;
+        let { resolveDir, css } = pluginData;
 
         return {
           resolveDir,
-          contents: compiledCss,
+          contents: css,
           loader: "css" as const,
         };
       });
